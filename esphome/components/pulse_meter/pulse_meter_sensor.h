3f03323eabbf901538283b2f34e8f93ad4da2fa1
Petr Kejval <petr.kejval6@gmail.com>
2025-02-17 13:44:04 +0100

    fds

--- esphome/components/pulse_meter/pulse_meter_sensor.h
+++ esphome/components/pulse_meter/pulse_meter_sensor.h
@@ -7,7 +7,7 @@
 #include "driver/pcnt.h"
 #include "soc/pcnt_struct.h"
 #include "esp_timer.h"
-#include <esp32-hal-timer.h>
+#include "driver/timer.h"
 
 namespace esphome {
 namespace pulse_meter {
