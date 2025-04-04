4a99beb57bd9a2be8256c6ffa126e7940a9dae57
Petr Kejval <petr.kejval6@gmail.com>
2025-04-04 09:20:53 +0200

    support AP mode

--- esphome/components/wifi/wifi_component.cpp
+++ esphome/components/wifi/wifi_component.cpp
@@ -721,8 +721,9 @@ bool WiFiComponent::can_proceed() {
 }
 void WiFiComponent::set_reboot_timeout(uint32_t reboot_timeout) { this->reboot_timeout_ = reboot_timeout; }
 bool WiFiComponent::is_connected() {
-  return this->state_ == WIFI_COMPONENT_STATE_STA_CONNECTED &&
-         this->wifi_sta_connect_status_() == WiFiSTAConnectStatus::CONNECTED && !this->error_from_callback_;
+  return (this->state_ == WIFI_COMPONENT_STATE_STA_CONNECTED &&
+          this->wifi_sta_connect_status_() == WiFiSTAConnectStatus::CONNECTED && !this->error_from_callback_) ||
+         this->state_ == WIFI_COMPONENT_STATE_AP;
 }
 void WiFiComponent::set_power_save_mode(WiFiPowerSaveMode power_save) { this->power_save_ = power_save; }
 
