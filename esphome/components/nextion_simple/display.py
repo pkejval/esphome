f7be4e253bb62ba9a85669ce23986a51a7d33021
Petr Kejval <petr.kejval6@gmail.com>
2025-09-10 08:17:45 +0200

    fix

--- esphome/components/nextion_simple/display.py
+++ esphome/components/nextion_simple/display.py
@@ -2,6 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import automation
 from esphome.components import uart
+from esphome.const import CONF_ID, CONF_UART_ID, CONF_TRIGGER_ID
 
 DEPENDENCIES = ["uart"]
 
@@ -11,12 +12,6 @@ NextionSetupTrigger = nextion_simple_ns.class_("NextionSetupTrigger", automation
 NextionPageTrigger = nextion_simple_ns.class_("NextionPageTrigger", automation.Trigger.template())
 NextionReadyTrigger = nextion_simple_ns.class_("NextionReadyTrigger", automation.Trigger.template())
 
-CONF_COMPONENT_NAME = "objname"
-CONF_VALUE = "value"
-CONF_COLOR = "color"
-CONF_PAGE = "page"
-CONF_TEXT = "text"
-CONF_ARGS = "args"
 CONF_TFT_URL = "tft_url"
 CONF_ON_SETUP = "on_setup"
 CONF_ON_PAGE = "on_page"
@@ -25,19 +20,31 @@ CONF_NEXTION_READY_COOLDOWN = "nextion_ready_cooldown"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(NextionSimple),
-    cv.Required("uart_id"): cv.use_id(uart.UARTComponent),
+    cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
     cv.Optional(CONF_TFT_URL): cv.string,
+
+    # on_setup nemá parametry → jde to přímo přes callback registrátor
     cv.Optional(CONF_ON_SETUP): automation.validate_automation(single=True),
-    cv.Optional(CONF_ON_PAGE): automation.validate_automation(single=True),
-    cv.Optional(CONF_ON_NEXTION_READY): automation.validate_automation(single=True),
+
+    # on_page má parametr (int page) → definuj trigger_id
+    cv.Optional(CONF_ON_PAGE): automation.validate_automation({
+        cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(NextionPageTrigger),
+    }),
+
+    # on_nextion_ready bez parametrů → taky trigger s id
+    cv.Optional(CONF_ON_NEXTION_READY): automation.validate_automation({
+        cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(NextionReadyTrigger),
+    }),
+
+    # POZOR: default musí mít jednotku
     cv.Optional(CONF_NEXTION_READY_COOLDOWN, default="500ms"): cv.positive_time_period_milliseconds,
 })
 
 async def to_code(config):
-    var = cg.new_Pvariable(config["id"])
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    uart_par = await cg.get_variable(config["uart_id"])
+    uart_par = await cg.get_variable(config[CONF_UART_ID])
     cg.add(var.set_uart_parent(uart_par))
 
     if CONF_TFT_URL in config:
@@ -46,19 +53,21 @@ async def to_code(config):
     if CONF_NEXTION_READY_COOLDOWN in config:
         cg.add(var.set_nextion_ready_cooldown(int(config[CONF_NEXTION_READY_COOLDOWN].total_milliseconds)))
 
-    # on_setup: máme přímo callback na parentu, není třeba vytvářet Trigger instanci
+    # on_setup – přímo přes registrátor
     if CONF_ON_SETUP in config:
         await automation.build_automation(var.add_on_setup_callback, [], config[CONF_ON_SETUP])
 
-    # on_page: vytvoř anonymní trigger instanci s konstruktorem (parent = var)
+    # on_page – vytvoř Trigger instanci s daným ID a parentem var
     if CONF_ON_PAGE in config:
-        trig = cg.new_Pvariable(nextion_simple_ns.class_("NextionPageTrigger"), var)
-        await automation.build_automation(trig, [(cg.int_, "page")], config[CONF_ON_PAGE])
+        for conf in config[CONF_ON_PAGE]:
+            trig = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
+            await automation.build_automation(trig, [(cg.int_, "page")], conf)
 
-    # on_nextion_ready: totéž – žádné stringové jméno, rovnou instance s parentem
+    # on_nextion_ready – bez parametrů, ale stejně Trigger s ID
     if CONF_ON_NEXTION_READY in config:
-        trig = cg.new_Pvariable(nextion_simple_ns.class_("NextionReadyTrigger"), var)
-        await automation.build_automation(trig, [], config[CONF_ON_NEXTION_READY])
+        for conf in config[CONF_ON_NEXTION_READY]:
+            trig = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
+            await automation.build_automation(trig, [], conf)
 
 # Actions
 SetComponentValueAction = nextion_simple_ns.class_("SetComponentValueAction", automation.Action.template())
@@ -70,5 +79,3 @@ SetComponentBackgroundColorAction = nextion_simple_ns.class_("SetComponentBackgr
 SetComponentFontColorAction = nextion_simple_ns.class_("SetComponentFontColorAction", automation.Action.template())
 SetComponentVisibilityAction = nextion_simple_ns.class_("SetComponentVisibilityAction", automation.Action.template())
 SetPageAction = nextion_simple_ns.class_("SetPageAction", automation.Action.template())
-
-# You can extend with YAML action schemas as in your original if needed.
